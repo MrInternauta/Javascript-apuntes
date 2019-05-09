095bec366c346398e61cd5cffd031545c0550c14
Felipe de Jesus Ramirez Rodriguez <feldjesus@gmail.com>
2019-05-08 23:29:03 -0500

    Update request.async_await.js

--- request.async_await.js
+++ request.async_await.js
@@ -1,16 +1,18 @@
+const fetch = require("node-fetch");
 
-function handleError(err){
-  console.log(`ERROR: ${err}`)
+function handleError(err) {
+    console.log(`ERROR: ${err}`)
 }
-async function getluke(){
-  try{
-    const response = await fetch('https://swapi.co/api/people/1/')
- 	const luke = await response.json()   
-    const responseHomeworld = await fetch(luke.homeworld)
-    luke.homeworld = await responseHomeworld.json()
-  	console.log(`Luke nacio en ${luke.homeworld.name}`)
-  }catch(error){
-  	handleError(error)
-  }  
+async function getluke() {
+    try {
+        const response = await fetch('https://swapi.co/api/people/1/')
+        const luke = await response.json()
+        const responseHomeworld = await fetch(luke.homeworld)
+        luke.homeworld = await responseHomeworld.json()
+        console.log(`Luke nacio en ${luke.homeworld.name}`)
+    } catch (error) {
+        handleError(error)
+    }
 }
 
+getluke()
